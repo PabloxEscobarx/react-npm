2f4e46214500241794562a19584452f491a114e6
PabloxEscobar <pavelx1997@gmail.com>
2021-11-11 20:43:17 +0200

    homework

---
+++ index.js
@@ -0,0 +1,8 @@
+const plus = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+plus(5, 4);
+minus(5, 4);
+divide(5, 4);
+multiply(5, 4);
